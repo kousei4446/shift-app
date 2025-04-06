8d6ee82f72e62bf421285c1b4d7adf301533df8b
EC2 Default User <ec2-user@ip-10-0-10-29.ap-northeast-1.compute.internal>
2025-04-06 08:27:24 +0000

    refactor

--- config/cors.php
+++ config/cors.php
@@ -16,7 +16,8 @@ return [
     */
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:8000', 'http://localhost:8000'],
+    //'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:8000', 'http://localhost:8000'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
